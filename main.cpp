e57934f1aa77c6b1ceb7bc337c1d6d603b578070
Harrison Balogh <harrisonbalogh@yahoo.com>
2017-09-19 16:55:00 -0700

    Main Template
    Generated main.cpp from Xcode project.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  444_project
+//
+//  Created by Harrison Balogh on 9/19/17.
+//  Copyright © 2017 Harxer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
