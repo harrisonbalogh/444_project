92843f5532f9faa47dbf1a72afaeac454593562b
Harrison Balogh <harrisonbalogh@yahoo.com>
2017-09-24 16:58:23 -0700

    All Tokens Implemented
    • Fixed new line mistake. Same fix that mark discovered. New lines now
    delimit tokens.
    • Program can now parse the following additional tokens:
      - Int
      - Float
      - DoubleQuote-Braced strings
      - Variable ID’s
      - Comments

--- main.cpp
+++ main.cpp
@@ -5,41 +5,49 @@
 //  Created by Harrison Balogh on 9/19/17.
 //  Copyright © 2017 Harxer. All rights reserved.
 //
+
 #include <iostream>
 #include <fstream>
 #include <map>
 #include <string>
+#include <cmath>
+
 using namespace std;
 
 // Functions Prototypes
-string readFile();
+string readFile(); // Temporary method for reading the input
 void parseTokens(string input);
 int getTokenID(string word);
 void loadGrammarMap();
 void loadGrammarDescriptorMap();
+bool isDelimiter(char character);
 
 map<int, string> REGEX_GRAMMAR;
-map<int, string> REGEX_DESCRIPTOR_GRAMMAR;
-int line_num = 1;
+map<int, string> REGEX_GRAMMAR_DESCRIPTOR;
 
 int main(int argc, const char * argv[]) {
-
+    
     loadGrammarMap();
     loadGrammarDescriptorMap();
+    
     string input = readFile();
+    
     parseTokens(input);
+    
+    cout << endl;
     return 0;
 }
 
-// Read in from file
+
+/// Read in from file
 string readFile() {
     string line;
     string buildString = "";
-    ifstream fileIn ("/Users/marktan/Documents/in.txt");
-
+    ifstream fileIn ("/Users/harrisonbalogh/Developer/444_project/in.txt");
+    
     if (fileIn.is_open()) {
         while ( getline(fileIn, line)) {
-            buildString += line + '\n';
+            buildString += line + "\n";
         }
     }
     else {
@@ -49,88 +57,211 @@ string readFile() {
     return buildString;
 }
 
-// Parse tokens within an input
+/// Parse token records from provided input.
 void parseTokens(string input) {
-
+    
     string buildToken = "";
-
+    // This will allow iterator below to skip all remaining characters in a line if it encounters '//'
+    bool commentedLine = false;
+    
+    // Iterate through all input
     for (int c = 0; c < strlen(input.c_str()); c++) {
-        if (input[c] == ' ' || input[c] == '\n') {
-
-            // Whitespace - token delimited
+        
+        // Don't read characters in line after determining it is a comment.
+        if (commentedLine) {
+            // Toggle commentedLine flag if it is true and reached end of line.
+            if ( input[c] == '\n') {
+                buildToken = "";
+                commentedLine = false;
+            }
+            continue;
+        }
+        
+        // Check if this is a doublequote-braced string
+        if (!buildToken.empty()) {
+            // Check for a started string of text
+            if (buildToken[0] == '\"') {
+                buildToken += input[c];
+                // Check for closing doublequote
+                if (input[c] == '\"' && c != 0) {
+                    
+                    cout << "'" << buildToken << "' is " << REGEX_GRAMMAR_DESCRIPTOR[5] << endl;
+                    
+                    // Flush buildToken
+                    buildToken = "";
+                }
+                continue;
+            }
+        }
+        
+        // Token delimited if whitespace, new line, or semicolon
+        if ( isDelimiter( input[c]) ) {
+            
+            // Skip if buildToken is empty
+            if (buildToken.empty()) {
+                
+                // Output the delimiter if it isn't whitespace or end line
+                if ( input[c] != ' ' && input[c] != '\n') {
+                    int delimiterID = getTokenID(string({input[c]}));
+                    cout << "'" << input[c] << "' is " << REGEX_GRAMMAR_DESCRIPTOR[delimiterID] << endl;
+                }
+                
+                continue;
+            }
+            
+            cout << "'" << buildToken << "' is ";
+            
             int tokenID = getTokenID(buildToken);
-            cout << "(Tok: \t" << tokenID;
-            cout << "\tline= " << line_num; //line_num needs to increment with each following line, will fix later
-
-            if (tokenID == 0)
-                cout << "\tstr= \"" << buildToken << "\")";
-            else if (tokenID == 5)
-            	cout << "\tstr= " << buildToken << ")";
-            else if (tokenID == 3)
-            	cout << "\tstr= \"" << buildToken << "\" \t" << "int= " << buildToken << ")";
-            	// 2nd buildToken should be converted to int
-            else if (tokenID == 4)
-            	cout << "\tstr= \"" << buildToken << "\" \t" << "float= " << buildToken << ")";
-            	// 2nd buildToken should be converted to int
-            else
-                cout << "\tstr= "  << REGEX_DESCRIPTOR_GRAMMAR[tokenID] << ")";
-
+            
+            // Output token
+            cout << REGEX_GRAMMAR_DESCRIPTOR[tokenID] << endl;
+            
+            // Output the delimiter if it isn't whitespace or end line
+            if ( input[c] != ' ' && input[c] != '\n') {
+                int delimiterID = getTokenID(string({input[c]}));
+                cout << "'" << input[c] << "' is " << REGEX_GRAMMAR_DESCRIPTOR[delimiterID] << endl;
+            }
+            
             // Flush builder
             buildToken = "";
-	    if(input[c] == '\n')
-            	line_num++;
-            cout << endl;
+            
         } else {
+            
+            // Character is not a delimiter but check if its a comment
+            if (buildToken.length() > 0) {
+                // 2 slashes in a row indicates a comment. Stop reading rest of line.
+                if (buildToken[buildToken.length()-1] == '/' && input[c] == '/') {
+                    commentedLine = true;
+                    // Output found comment token
+                    cout << "'//' is comment" << endl;
+                    continue;
+                }
+            }
+            
             buildToken += input[c];
         }
     }
-    if (buildToken.compare("")) {
+    
+    if (!buildToken.empty()) {
         // Leftover token after for-loop finishes
+        cout << "'" << buildToken << "' is ";
+        
         int tokenID = getTokenID(buildToken);
-        cout << "(Tok: \t" << tokenID;
-        cout << "\tline= " << line_num;
-
-        if (tokenID == 0)
-            cout << "\tstr= \"" << buildToken << "\")";
-        else if (tokenID == 5)
-        	cout << "\tstr= " << buildToken << ")";
-        else if (tokenID == 3)
-        	cout << "\tstr= \"" << buildToken << "\" \t" << "int= " << buildToken << ")";
-        	// 2nd buildToken should be converted to int
-        else if (tokenID == 4)
-        	cout << "\tstr= \"" << buildToken << "\" \t" << "float= " << buildToken << ")";
-        	// 2nd buildToken should be converted to int
-        else
-            cout << "\tstr= " << REGEX_DESCRIPTOR_GRAMMAR[tokenID] << ")";
+        
+        // Output token
+        cout << REGEX_GRAMMAR_DESCRIPTOR[tokenID] << endl;
     }
     cout << endl;
 }
 
 /// Looks up the given word and returns its ID in the GRAMMAR table.
 int getTokenID(string word) {
-
+    
     map<int, string>::const_iterator it;
+    // Lookup rule matching word. Only keywords, paired delimiters, punctuation, operators
     for (it = REGEX_GRAMMAR.begin(); it != REGEX_GRAMMAR.end(); ++it) {
+        
         if (!word.compare(it -> second)) {
             return it -> first;
         }
+        
     }
+    
+    // Note if its a negative number for integer/float parsing
+    bool negativePrefix = false;
+    // Check for minus sign (plus sign is default)
+    if (word.length() >= 2) {
+        if (word[0] == '-') {
+            negativePrefix = true;
+        }
+        if (word[0] == '+' || word[0] == '-') {
+            // Truncate word to not include prefix
+            word = word.substr(1);
+        }
+    }
+    
+    // Check if its an ID
+    if (word.length() > 0) {
+        // First character indicates if its an ID
+        if ((word[0] >= 'A' && word[0] <= 'Z') || (word[0] >= 'a' && word[0] <= 'z') || word[0] == '_') {
+            return 2;
+        }
+    }
+    
+    int floatAccuracy = -1; // Indicates the digit place of float. -1 means it's not a float
+    float numberBuilder = -1; // -1 Indicates that no digits have yet been found
+    
+    for (int c = 0; c < word.length(); c++) {
+        
+        // Check if encounted a digit
+        if (word[c] >= '0' && word[c] <= '9') {
+            
+            // Indicate when first digit has been found
+            if (numberBuilder == -1)
+                numberBuilder = 0;
+            
+            // Check if it's a float. If -1 is true, it's an Int
+            if (floatAccuracy != -1) {
+                
+                // Increment float accuracy after adding current decimal place
+                numberBuilder += (1 / pow(10, floatAccuracy++)) * (word[c] - '0');
+                
+            } else {
+                // Shift current number and add new digit
+                numberBuilder = numberBuilder * 10 + (word[c] - '0');
+            }
+
+        // Check if encountered a '.' and there are digits before the '.' and there wasn't already a '.'
+        } else  if (word[c] == '.' && numberBuilder != -1 && floatAccuracy == -1) {
+            
+            floatAccuracy = 1;
+            
+        } else {
+            
+            // Character wasn't recognized, not a float or int.
+            numberBuilder = -1; // indicate that no number was found
+            break;
+        
+        }
+    }
+    
+    // Check if number was parsed
+    if (numberBuilder != -1) {
+        
+        // Int ID: 3, Float ID: 4
+        return (floatAccuracy == -1) ? 3 : 4;
+        
+//        return negativePrefix ? -numberBuilder : numberBuilder;
+    }
+    
     // Did not find token ID.
-    return 0;
+    return 99; // Unknown Token ID
 }
 
-//Loads grammap into a map
+/// Returns true if the given character
+bool isDelimiter(char character) {
+    if (character == ' ') {
+        return true;
+    } else if (character == ';') {
+        return true;
+    } else if (character == ',') {
+        return true;
+    } else if (character == '\n') {
+        return true;
+    }
+    return false;
+}
+
+/// Push a record to the console after it has been read in by parseTokens()
+//void outputToken() {
+    // output all token records, one per line.
+//}
+
 void loadGrammarMap() {
-
-	REGEX_GRAMMAR[2] = "";	// identifier
-	REGEX_GRAMMAR[3] = "";	// integer
-	REGEX_GRAMMAR[4] = "";	// float
-	REGEX_GRAMMAR[5] = "";	// string
-
-	// Unpaired Delimiters
+    // Unpaired Delimiters
     REGEX_GRAMMAR[6] = ","; // comma
     REGEX_GRAMMAR[7] = ";"; // semi
-
+    
     // Keywords
     REGEX_GRAMMAR[10] = "prog";     // kprog
     REGEX_GRAMMAR[11] = "main";     // kmain
@@ -155,7 +286,7 @@ void loadGrammarMap() {
     REGEX_GRAMMAR[36] = "]";        // bracket2
     REGEX_GRAMMAR[37] = "(";        // parens1
     REGEX_GRAMMAR[38] = ")";        // parens2
-
+    
     // Other punctuation
     REGEX_GRAMMAR[41] = "*";        // aster
     REGEX_GRAMMAR[42] = "^";        // caret
@@ -165,7 +296,7 @@ void loadGrammarMap() {
     REGEX_GRAMMAR[46] = "-";        // minus
     REGEX_GRAMMAR[47] = "+";        // plus
     REGEX_GRAMMAR[48] = "/";        // splash
-
+    
     // Multi-char operators
     REGEX_GRAMMAR[51] = "->";       // oparrow
     REGEX_GRAMMAR[52] = "==";       // opeq
@@ -174,63 +305,84 @@ void loadGrammarMap() {
     REGEX_GRAMMAR[55] = ">=";       // opge
     REGEX_GRAMMAR[56] = "<<";       // opshl
     REGEX_GRAMMAR[57] = ">>";       // opshr
-
+    
     // Miscellaneous
-    REGEX_GRAMMAR[99] = "";			//error
-    //REGEX_GRAMMAR[0] = "";			//eof
+    
 }
 
-//Load descriptions for grammar in a map
 void loadGrammarDescriptorMap() {
+    
+    // Special words
+    REGEX_GRAMMAR_DESCRIPTOR[2] = "id";         // .*
+    REGEX_GRAMMAR_DESCRIPTOR[3] = "int";        // +/- & DIGITS
+    REGEX_GRAMMAR_DESCRIPTOR[4] = "float";      // +/- & DIGITS . DIGITS
+    REGEX_GRAMMAR_DESCRIPTOR[5] = "string";     // '"' .* '"'
+    
     // Unpaired Delimiters
-    REGEX_DESCRIPTOR_GRAMMAR[6] = "comma";      // ,
-    REGEX_DESCRIPTOR_GRAMMAR[7] = "semi";       // ;
-
+    REGEX_GRAMMAR_DESCRIPTOR[6] = "comma";      // ,
+    REGEX_GRAMMAR_DESCRIPTOR[7] = "semi";       // ;
+    
     // Keywords
-    REGEX_DESCRIPTOR_GRAMMAR[10] = "kprog";     // prog
-    REGEX_DESCRIPTOR_GRAMMAR[11] = "kmain";     // main
-    REGEX_DESCRIPTOR_GRAMMAR[12] = "kfcn";      // fcn
-    REGEX_DESCRIPTOR_GRAMMAR[13] = "kclass";    // class
-    REGEX_DESCRIPTOR_GRAMMAR[15] = "kfloat";    // float
-    REGEX_DESCRIPTOR_GRAMMAR[16] = "kint";      // int
-    REGEX_DESCRIPTOR_GRAMMAR[17] = "kstring";   // string
-    REGEX_DESCRIPTOR_GRAMMAR[18] = "kif";       // if
-    REGEX_DESCRIPTOR_GRAMMAR[19] = "kelseif";   // elseif
-    REGEX_DESCRIPTOR_GRAMMAR[20] = "kelse";     // else
-    REGEX_DESCRIPTOR_GRAMMAR[21] = "kwhile";    // while
-    REGEX_DESCRIPTOR_GRAMMAR[22] = "kinput";    // input
-    REGEX_DESCRIPTOR_GRAMMAR[23] = "kprint";    // print
-    REGEX_DESCRIPTOR_GRAMMAR[24] = "knew";      // new
-    REGEX_DESCRIPTOR_GRAMMAR[25] = "kreturn";   // return
-    REGEX_DESCRIPTOR_GRAMMAR[31] = "angle1";    // <
-    REGEX_DESCRIPTOR_GRAMMAR[32] = "angle2";    // >
-    REGEX_DESCRIPTOR_GRAMMAR[33] = "brace1";    // {
-    REGEX_DESCRIPTOR_GRAMMAR[34] = "brace2";    // }
-    REGEX_DESCRIPTOR_GRAMMAR[35] = "bracket1";  // [
-    REGEX_DESCRIPTOR_GRAMMAR[36] = "bracket2";  // ]
-    REGEX_DESCRIPTOR_GRAMMAR[37] = "parens1";   // (
-    REGEX_DESCRIPTOR_GRAMMAR[38] = "parens2";   // )
-
+    REGEX_GRAMMAR_DESCRIPTOR[10] = "kprog";     // prog
+    REGEX_GRAMMAR_DESCRIPTOR[11] = "kmain";     // main
+    REGEX_GRAMMAR_DESCRIPTOR[12] = "kfcn";      // fcn
+    REGEX_GRAMMAR_DESCRIPTOR[13] = "kclass";    // class
+    REGEX_GRAMMAR_DESCRIPTOR[15] = "kfloat";    // float
+    REGEX_GRAMMAR_DESCRIPTOR[16] = "kint";      // int
+    REGEX_GRAMMAR_DESCRIPTOR[17] = "kstring";   // string
+    REGEX_GRAMMAR_DESCRIPTOR[18] = "kif";       // if
+    REGEX_GRAMMAR_DESCRIPTOR[19] = "kelseif";   // elseif
+    REGEX_GRAMMAR_DESCRIPTOR[20] = "kelse";     // else
+    REGEX_GRAMMAR_DESCRIPTOR[21] = "kwhile";    // while
+    REGEX_GRAMMAR_DESCRIPTOR[22] = "kinput";    // input
+    REGEX_GRAMMAR_DESCRIPTOR[23] = "kprint";    // print
+    REGEX_GRAMMAR_DESCRIPTOR[24] = "knew";      // new
+    REGEX_GRAMMAR_DESCRIPTOR[25] = "kreturn";   // return
+    REGEX_GRAMMAR_DESCRIPTOR[31] = "angle1";    // <
+    REGEX_GRAMMAR_DESCRIPTOR[32] = "angle2";    // >
+    REGEX_GRAMMAR_DESCRIPTOR[33] = "brace1";    // {
+    REGEX_GRAMMAR_DESCRIPTOR[34] = "brace2";    // }
+    REGEX_GRAMMAR_DESCRIPTOR[35] = "bracket1";  // [
+    REGEX_GRAMMAR_DESCRIPTOR[36] = "bracket2";  // ]
+    REGEX_GRAMMAR_DESCRIPTOR[37] = "parens1";   // (
+    REGEX_GRAMMAR_DESCRIPTOR[38] = "parens2";   // )
+    
     // Other punctuation
-    REGEX_DESCRIPTOR_GRAMMAR[41] = "aster";     // *
-    REGEX_DESCRIPTOR_GRAMMAR[42] = "caret";     // ^
-    REGEX_DESCRIPTOR_GRAMMAR[43] = "colon";     // :
-    REGEX_DESCRIPTOR_GRAMMAR[44] = "dot";       // .
-    REGEX_DESCRIPTOR_GRAMMAR[45] = "equals";    // =
-    REGEX_DESCRIPTOR_GRAMMAR[46] = "minus";     // -
-    REGEX_DESCRIPTOR_GRAMMAR[47] = "plus";      // +
-    REGEX_DESCRIPTOR_GRAMMAR[48] = "splash";    // /
-
+    REGEX_GRAMMAR_DESCRIPTOR[41] = "aster";     // *
+    REGEX_GRAMMAR_DESCRIPTOR[42] = "caret";     // ^
+    REGEX_GRAMMAR_DESCRIPTOR[43] = "colon";     // :
+    REGEX_GRAMMAR_DESCRIPTOR[44] = "dot";       // .
+    REGEX_GRAMMAR_DESCRIPTOR[45] = "equals";    // =
+    REGEX_GRAMMAR_DESCRIPTOR[46] = "minus";     // -
+    REGEX_GRAMMAR_DESCRIPTOR[47] = "plus";      // +
+    REGEX_GRAMMAR_DESCRIPTOR[48] = "splash";    // /
+    
     // Multi-char operators
-    REGEX_DESCRIPTOR_GRAMMAR[51] = "oparrow";    // ->
-    REGEX_DESCRIPTOR_GRAMMAR[52] = "opeq";       // ==
-    REGEX_DESCRIPTOR_GRAMMAR[53] = "opne";       // !=
-    REGEX_DESCRIPTOR_GRAMMAR[54] = "ople";       // <=
-    REGEX_DESCRIPTOR_GRAMMAR[55] = "opge";       // >=
-    REGEX_DESCRIPTOR_GRAMMAR[56] = "opshl";      // <<
-    REGEX_DESCRIPTOR_GRAMMAR[57] = "opshr";      // >>
-
+    REGEX_GRAMMAR_DESCRIPTOR[51] = "oparrow";    // ->
+    REGEX_GRAMMAR_DESCRIPTOR[52] = "opeq";       // ==
+    REGEX_GRAMMAR_DESCRIPTOR[53] = "opne";       // !=
+    REGEX_GRAMMAR_DESCRIPTOR[54] = "ople";       // <=
+    REGEX_GRAMMAR_DESCRIPTOR[55] = "opge";       // >=
+    REGEX_GRAMMAR_DESCRIPTOR[56] = "opshl";      // <<
+    REGEX_GRAMMAR_DESCRIPTOR[57] = "opshr";      // >>
+    
     // Miscellaneous
-    REGEX_DESCRIPTOR_GRAMMAR[99] = "error";		//Unknown token
-    //REGEX_DESCRIPTOR_GRAMMAR[0] = "eof";		//end of file
+    REGEX_GRAMMAR_DESCRIPTOR[99] = "error";      // Unknown Token
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
