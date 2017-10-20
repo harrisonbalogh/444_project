1c723c0ab16f68d222cc64e9f9ef5e5d5fcf70a0
angelsfly1992 <angelsfly1992@yahoo.com>
2017-10-19 17:27:10 -0700

    Create rule.cpp

---
+++ rule.cpp
@@ -0,0 +1,41 @@
+
+
+
+void Parser_map();
+map <int,string> Parser_map;
+using namespace std;
+void Parser_map () {
+Parser_map[] = "BBlock";        //Pgm
+Parser_map[] = "Vargroup";      //BBlock
+Parser_map[] = "PPVarlist";     //Vargroup
+Parser_map[] = "Vardecl";       //Varlist
+Parser_map[] = "Varlist";       //PPvarlist, Varlist
+Parser_map[] = "Stmt";          //Stmts
+Parser_map[] = "Stmts";         //Stmts
+Parser_map[] = "Varid";         //stasgn
+Parser_map[] = "PPexpr1";       //stprint
+Parser_map[] = "Expr";          //PPexpr1
+Parser_map[] = "Fact";          //
+Parser_map[] = "Rterm";         //
+Parser_map[] = "Exprlist";      //PPexprs, Moreexprs
+Parser_map[] = "Basekind";      //Vardecl
+Parser_map[] = "Opadd";         //Rterm
+Parser_map[] = "Term";          //
+Parser_map[] = "Oprel";         //Expr
+Parser_map[] = "Opmul";         //Term
+Parser_map[] = "Lthan";         //Oprel
+Parser_map[] = "Gthan";         //Oprel
+Parser_map[] = "kwdvars";       //Vargroup
+Parser_map[] = "'int'";         //Basekind
+Parser_map[] = "'float'";       //Basekind
+Parser_map[] = "'string'";      //Basekind
+Parser_map[] = "Moreexprs"      //Exprlist
+Parser_map[] = "id"             //Varid
+Parser_map[] = "E";             //E, Expr
+Parser_map[] = "R";             //R, Rterm
+Parser_map[] = "T";             //T, Term
+Parser_Map[] = "eps"; */
+}
+
+
+
