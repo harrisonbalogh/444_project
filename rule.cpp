b5eb97eff688637458d6e258d81e4a9016864c16
Mark Tan <damtan1996@gmail.com>
2017-10-24 20:32:30 -0700

    Updated rule maps
    Now the rule maps reflect the row and column names of the parser table. This will allow us to access the parser table entries more easily

--- rule.cpp
+++ rule.cpp
@@ -13,91 +13,65 @@ map<int, string> Parser_map;
 class Rule {
 	//Map all Parser Rules
 	void parserMap() {
-		Parser_map[] = "BBlock";        //Pgm
-		Parser_map[] = "Vargroup";      //BBlock
-		Parser_map[] = "PPVarlist";     //Vargroup
-		Parser_map[] = "Vardecl";       //Varlist
-		Parser_map[] = "Varlist";       //PPvarlist, Varlist
-		Parser_map[] = "Stmt";          //Stmts
-		Parser_map[] = "Stmts";         //Stmts
-		Parser_map[] = "Varid";         //stasgn
-		Parser_map[] = "PPexpr1";       //stprint
-		Parser_map[] = "Expr";          //PPexpr1
-		Parser_map[] = "Fact";          //T, Term,
-		Parser_map[] = "Rterm";         //E, Expr
-		Parser_map[] = "Exprlist";      //PPexprs, Moreexprs
-		Parser_map[] = "Basekind";      //Vardecl
-		Parser_map[] = "Opadd";         //Rterm
-		Parser_map[] = "Term";          //R, Rterm
-		Parser_map[] = "Oprel";         //Expr
-		Parser_map[] = "Opmul";         //Term
-		Parser_map[] = "Lthan";         //Oprel
-		Parser_map[] = "Gthan";         //Oprel
-		Parser_map[] = "kwdvars";       //Vargroup
-		Parser_map[] = "kwdwhile";      //Stwhile
-		Parser_map[] = "kwdprog";       //PGM
-		Parser_map[] = "'int'";         //Basekind
-		Parser_map[] = "'float'";       //Basekind
-		Parser_map[] = "'string'";      //Basekind
-		Parser_map[] = "Moreexprs";      //Exprlist
-		Parser_map[] = "id";             //Varid
-		Parser_map[] = "E";             //E, Expr
-		Parser_map[] = "R";             //R, Rterm
-		Parser_map[] = "T";             //T, Term
-		Parser_map[] = "Stwhile";       //Stmt
-		Parser_map[] = "Stprint";       //Stmt
-		Parser_map[] = "Stasgn";        //Stmt
-		Parser_map[] = "eps";           //E, R, T, Moreexprs, Stmts, Varlist, Vargroup
+		Parser_map[1] = "Pgm";
+		Parser_map[2] = "BBlock";
+		Parser_map[3] = "Vargroup";
+		Parser_map[4] = "PPVarlist";
+		Parser_map[5] = "Varlist";
+		Parser_map[6] = "Vardecl";
+		Parser_map[7] = "Basekind";
+		Parser_map[8] = "Varid";
+		Parser_map[9] = "Stmts";
+		Parser_map[10] = "Stmt";
+		Parser_map[11] = "Stasgn";
+		Parser_map[12] = "Stprint";
+		Parser_map[13] = "Stwhile";
+		Parser_map[14] = "PPexprs";
+		Parser_map[15] = "PPexpr1";
+		Parser_map[16] = "Exprlist";
+		Parser_map[17] = "Moreexprs";
+		Parser_map[18] = "E";
+		Parser_map[19] = "Expr";
+		Parser_map[20] = "R";
+		Parser_map[21] = "Rterm";
+		Parser_map[22] = "T";
+		Parser_map[23] = "Term";
+		Parser_map[24] = "Fact";
+		Parser_map[25] = "Oprel";
+		Parser_map[26] = "Lthan";
+		Parser_map[27] = "Gthan";
+		Parser_map[28] = "Opadd";
+		Parser_map[29] = "Opmul";
 	}
 
 	/// Maps all REGEX grammar rules with their ID numbers.
 	void loadGrammarMap() {
-		// Unpaired Delimiters
-		REGEX_GRAMMAR[6] = ","; // comma
-		REGEX_GRAMMAR[7] = ";"; // semi
-
-								// Keywords
-		REGEX_GRAMMAR[10] = "prog";     // kprog
-		REGEX_GRAMMAR[11] = "main";     // kmain
-		REGEX_GRAMMAR[12] = "fcn";      // kfcn
-		REGEX_GRAMMAR[13] = "class";    // kclass
-		REGEX_GRAMMAR[15] = "float";    // kfloat
-		REGEX_GRAMMAR[16] = "int";      // kint
-		REGEX_GRAMMAR[17] = "string";   // kstring
-		REGEX_GRAMMAR[18] = "if";       // kif
-		REGEX_GRAMMAR[19] = "elseif";   // kelseif
-		REGEX_GRAMMAR[20] = "else";     // kelse
-		REGEX_GRAMMAR[21] = "while";    // kwhile
-		REGEX_GRAMMAR[22] = "input";    // kinput
-		REGEX_GRAMMAR[23] = "print";    // kprint
-		REGEX_GRAMMAR[24] = "new";      // knew
-		REGEX_GRAMMAR[25] = "return";   // kreturn
-		REGEX_GRAMMAR[31] = "<";        // angle1
-		REGEX_GRAMMAR[32] = ">";        // angle2
-		REGEX_GRAMMAR[33] = "{";        // brace1
-		REGEX_GRAMMAR[34] = "}";        // brace2
-		REGEX_GRAMMAR[35] = "[";        // bracket1
-		REGEX_GRAMMAR[36] = "]";        // bracket2
-		REGEX_GRAMMAR[37] = "(";        // parens1
-		REGEX_GRAMMAR[38] = ")";        // parens2
-
-										// Other punctuation
-		REGEX_GRAMMAR[41] = "*";        // aster
-		REGEX_GRAMMAR[42] = "^";        // caret
-		REGEX_GRAMMAR[43] = ":";        // colon
-		REGEX_GRAMMAR[44] = ".";        // dot
-		REGEX_GRAMMAR[45] = "=";        // equals
-		REGEX_GRAMMAR[46] = "-";        // minus
-		REGEX_GRAMMAR[47] = "+";        // plus
-		REGEX_GRAMMAR[48] = "/";        // splash
-
-										// Multi-char operators
-		REGEX_GRAMMAR[51] = "->";       // oparrow
-		REGEX_GRAMMAR[52] = "==";       // opeq
-		REGEX_GRAMMAR[53] = "!=";       // opne
-		REGEX_GRAMMAR[54] = "<=";       // ople
-		REGEX_GRAMMAR[55] = ">=";       // opge
-		REGEX_GRAMMAR[56] = "<<";       // opshl
-		REGEX_GRAMMAR[57] = ">>";       // opshr
+		REGEX_GRAMMER[1] = "i";		//identifier
+		REGEX_GRAMMAR[2] = "prog";	//kprog
+		REGEX_GRAMMAR[3] = "vars";     	//vars
+		REGEX_GRAMMAR[4] = "print";    	//kprint
+		REGEX_GRAMMAR[5] = "while";    	//kwhile
+		REGEX_GRAMMAR[6] = "int";      	//kint
+		REGEX_GRAMMAR[7] = "float";    	//kfloat
+		REGEX_GRAMMAR[8] = "string";   	//kstring
+		REGEX_GRAMMAR[9] = ";"; 	// semi
+		REGEX_GRAMMAR[10] = ","; 	// comma
+		REGEX_GRAMMAR[11] = "=";        // opeq
+		REGEX_GRAMMAR[12] = "+";        // plus
+		REGEX_GRAMMAR[13] = "-";        // minus
+		REGEX_GRAMMAR[14] = "*";        // aster
+		REGEX_GRAMMAR[15] = "/";        // slash
+		REGEX_GRAMMAR[16] = "!=";       // opne
+		REGEX_GRAMMAR[17] = "<";        // angle1
+		REGEX_GRAMMAR[18] = "<=";       // ople
+		REGEX_GRAMMAR[19] = ">";        // angle2
+		REGEX_GRAMMAR[20] = ">=";       // opge
+		REGEX_GRAMMAR[21] = "^";        // caret
+		REGEX_GRAMMAR[22] = "[";        // bracket1
+		REGEX_GRAMMAR[23] = "]";        // bracket2
+		REGEX_GRAMMAR[24] = "(";        // parens1
+		REGEX_GRAMMAR[25] = ")";        // parens2
+		REGEX_GRAMMAR[26] = "{";        // brace1
+		REGEX_GRAMMAR[27] = "}";        // brace2
 	}
 };
