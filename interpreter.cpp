1a933f747a662feedb0d7624cbb108093070084c
Mark Tan <damtan1996@gmail.com>
2017-11-26 09:45:21 -0800

    Updated Functions and Pre-built AST in Main
    Filled out boolean functions and do_op function, as well as including a pre-made AST in main method.

--- interpreter.cpp
+++ interpreter.cpp
@@ -60,27 +60,61 @@ void add_entry(Node arn, Snode rsn) {
 }
 
 bool is_decl(Node arn) {
-	return true;
+	if(arn.get_data() == 'int' ||
+	   arn.get_data() == 'float' || arn.get_data() == 'string')
+		return true;
+	else
+		return false;
+
 }
 
 bool is_block(Node arn) {
-	return true;
+	if(arn.get_symbol() == '{')
+		return true;
+	else
+		return false;
 }
 
 bool is_use(Node arn) {
-	return true;
+	if(arn.get_symbol() == 'i')
+		return true;
+	else
+		return false;
 }
 
 bool is_op(Node arn) {
-	return true;
+	if(arn.get_symbol() == '+' || arn.get_symbol() == '-' ||
+	   arn.get_symbol() == "*" || arn.get_symbol() == '/')
+		return true;
+	else
+		return false;
 }
 
 void do_op(Node arn) {
-	//add
-	//sub
-	//mult
-	//divide
-	//compare
+	int newSym = 0;
+	Node n1 = arn.get_one_child(0);
+	Node n2 = arn.get_one_child(1);
+	stringstream s(n1.get_data());
+	stringstream t(n1.get_data());
+	int x,y;
+	s >> x;
+	t >> y;
+	char tester = arn.get_symbol();
+	switch(tester) {
+		case '+':
+			newSym = x + y;
+			break;
+		case '-':
+			newSym = x - y;
+			break;
+		case '*':
+			newSym = x * y;
+			break;
+		case '/':
+			newSym = x / y;
+			break;
+		default:
+			newSym = 0;
 }
 
 void A2S_use(Node ra, Snode rs) {
@@ -127,14 +161,35 @@ void AST2SCT(Node arn, SCTnode rsn) {
 }
 
 int main() {
+	//example: prog main { int B = ( 2 * 7 ) + 3 ; print ( B ) ; }
+	Node a,b,c,d,e,intB,q;
+	intB.set_id(8);	intB.set_symbol('i');	intB.set_data("B");
+	q.set_id(9);	q.set_symbol('=');	a.set_data("equals");
+	a.set_id(1);	a.set_symbol('+');	a.set_data("plus");
+	b.set_id(2);	a.set_symbol('*');	a.set_data("times");
+	c.set_id(3);	a.set_symbol('K');	a.set_data("3");
+	d.set_id(4);	a.set_symbol('K');	a.set_data("2");
+	e.set_id(5);	a.set_symbol('K');	a.set_data("7");
+	Node *f = &b;
+	Node *g = &c;
+	Node *h = &d;
+	Node *j = &e;
+	a.set_children();
+	a.set_child(f, 0);
+	a.set_child(g, 1);
+	b.set_children();
+	b.set_child(h, 0);
+	b.set_child(j, 1);
+	print_tree(n);
 
-	//example: prog main { int E = 12 ; int F = 0 ; F = E + E * E ; print ( E ) ; }
-	//obtain AST from parser
+	//look for brace, var decl (int, float, string), ID
+	//linkk i and K nodes to its scope node
+	//put ID into symbol table
+	//each scope node has a symbol table
+
 	//build SCT (using node that opens scope, decl, var)
 	//root will hold global identifiers
 	//SET UP ROOT BEFORE WALKING AST
-	//scope contains symbol table
-	//treewalk AST to run executable
 	
 	//decl should create new symtab in current scope node
 	//entry links back to AST decl
